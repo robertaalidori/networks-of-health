9c1fea7cd924440bdc74a4fe55964e28b9f50e89
roberta_alidori <roberta.alidori@unimi.it>
2025-07-08 12:14:16 +0200

    Minor changes to scripts/data

--- scripts/data.R
+++ scripts/data.R
@@ -2,20 +2,24 @@
 ## Import and clean all data.
 ## = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = 
 
-## Load & Rename Ego Data (da36975.0005.rda)
+# Load & Rename Ego Data (da36975.0005.rda)
 
 load("./data/da36975.0005.rda")  # Loads object da36975.0005
 
 ego_df <- get("da36975.0005")    # Rename to ego_df
-rm("da36975.0005")               # (Optional) Clean up old name
+rm("da36975.0005")               # Clean up old name
 
 save(ego_df, file = "./data/ego_df.rda")  # Save clean version
 
-## Load & Rename Alter Data (da36975.0001.rda)
+# Load & Rename Alter Data (da36975.0001.rda)
 load("./data/da36975.0001.rda")  # Loads object da36975.0001
 
 alter_df <- get("da36975.0001")  # Rename to alter_df
 
-rm("da36975.0001")               # (Optional) Clean up old name
+rm("da36975.0001")               # Clean up old name
 
 save(alter_df, file = "./data/alter_df.rda")  # Save clean version
+
+# Check the structure of the ego_df
+ego_df
+str(ego_df)
